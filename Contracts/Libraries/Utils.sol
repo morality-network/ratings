829d24bf7616c3d2d4a246437ce44e717f39a372
msharp19 <matt19sharp@gmail.com>
2022-09-17 18:25:04 +0800

    Fixed errors

--- Contracts/Libraries/Utils.sol
+++ Contracts/Libraries/Utils.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0.0;
 
+library Utils{
+
+    function getTimestamp() public view returns(uint256){
+        return block.timestamp;
+    }
+}
+
 library StringUtils{
 
     function compareStrings(string memory a, string memory b) public pure returns (bool) {
@@ -33,12 +40,5 @@ library UrlUtils{
         require(StringUtils.compareStrings(lastCharacter, "/") == false, "Url must not end with '/'");
 
         return true;
-    }
-}
-
-library TimeUtils{
-
-    function getTimestamp() public view returns(uint256){
-        return block.timestamp;
     }
 }
