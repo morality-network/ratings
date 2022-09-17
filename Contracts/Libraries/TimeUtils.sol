ea6bb3297734d63f0e6e5bcf0adcce3847b06d99
msharp19 <matt19sharp@gmail.com>
2022-09-17 18:32:29 +0800

    Fixed errors

--- Contracts/Libraries/TimeUtils.sol
+++ Contracts/Libraries/TimeUtils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0.0;
 
-library Utils{
+library TimeUtils{
 
     function getTimestamp() public view returns(uint256){
         return block.timestamp;
