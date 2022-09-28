c3da356f946045c2443b29d10594f9d27719910e
Matt Sharp <msharp19@users.noreply.github.com>
2022-09-28 14:00:10 +0800

    Delete TimeUtils.sol

--- Contracts/Libraries/TimeUtils.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.0.0;
-
-library TimeUtils{
-
-    function getTimestamp() public view returns(uint256){
-        return block.timestamp;
-    }
-}
